6662b745545ed28da06059b360675550cf76e2aa
letere-gzj <letere-gzj@users.noreply.github.com>
2024-08-27 17:57:07 +0800

    auto deploy letere-gzj/hugo-source@889d83eef910ae20cebbfda121d96017542ab387

--- live2d/my-waifu-tips.js
+++ live2d/my-waifu-tips.js
@@ -136,6 +136,15 @@
     };
 
     /**
+     * 随机选择模型
+     * @param obj
+     * @returns {*}
+     */
+    function randomSelection(obj) {
+        return Array.isArray(obj) ? obj[Math.floor(Math.random() * obj.length)] : obj;
+    }
+
+    /**
      * 按顺序选择数据
      * @param obj 数据
      * @param currentIdx 当前序号
@@ -190,6 +199,7 @@
             });
     }
 
+    let jsonData;
     /**
      * 加载组件
      * @param config 配置
@@ -224,6 +234,8 @@
         })();
 
         function registerEventListener(result) {
+            // json数据赋值
+            jsonData = result;
             // 检测用户活动状态，并在空闲时显示消息
             let userAction = false,
                 userActionTimer,
@@ -242,7 +254,7 @@
                     }, 20000);
                 }
             }, 1000);
-            showMessage(getWaifuWelcome(result.time), 7000, 11);
+            showWaifuWelcome();
             window.addEventListener("mouseover", event => {
                 for (let { selector, text } of result.mouseover) {
                     if (event.target.matches(selector)) {
@@ -251,13 +263,6 @@
                         showMessage(text, 4000, 8);
                         return;
                     }
-                    // if (!event.target.closest(selector)) continue;
-                    // if (lastHoverElement === selector) return;
-                    // lastHoverElement = selector;
-                    // text = randomSelection(text);
-                    // text = text.replace("{text}", event.target.innerText);
-                    // showMessage(text, 4000, 8);
-                    // return;
                 }
             });
             window.addEventListener("click", event => {
@@ -276,6 +281,7 @@
                 if ((after.split("/")[0] <= now.getMonth() + 1 && now.getMonth() + 1 <= before.split("/")[0]) && (after.split("/")[1] <= now.getDate() && now.getDate() <= before.split("/")[1])) {
                     text = randomSelection(text);
                     text = text.replace("{year}", now.getFullYear());
+                    console.log(123456)
                     messageArray.push(text);
                 }
             });
@@ -382,73 +388,53 @@
         }
     }
 
-    window.initWidget = initWidget;
-})()
-
-/**
- * 随机选择模型
- * @param obj
- * @returns {*}
- */
-function randomSelection(obj) {
-    return Array.isArray(obj) ? obj[Math.floor(Math.random() * obj.length)] : obj;
-}
-
-let messageTimer;
-/**
- * 展示信息
- * @param text 文本
- * @param timeout 展示时长
- * @param priority 优先级
- */
-function showWaifuTips(text, timeout, priority) {
-    if (!text || (sessionStorage.getItem("waifu-text") && sessionStorage.getItem("waifu-text") > priority)) return;
-    if (messageTimer) {
-        clearTimeout(messageTimer);
-        messageTimer = null;
-    }
-    text = randomSelection(text);
-    sessionStorage.setItem("waifu-text", priority);
-    const tips = document.getElementById("waifu-tips");
-    tips.innerHTML = text;
-    tips.classList.add("waifu-tips-active");
-    messageTimer = setTimeout(() => {
-        sessionStorage.removeItem("waifu-text");
-        tips.classList.remove("waifu-tips-active");
-    }, timeout);
-}
-
-/**
- * 获取欢迎信息
- * @param time 时间
- * @returns {*|string} 欢迎信息文本
- */
-function getWaifuWelcome(time) {
-    if (location.pathname === "/") { // 如果是主页
-        for (let { hour, text } of time) {
-            const now = new Date(),
-                after = hour.split("-")[0],
-                before = hour.split("-")[1] || after;
-            if (after <= now.getHours() && now.getHours() <= before) {
-                return text;
+    /**
+     * 获取欢迎信息
+     * @returns {*|string} 欢迎信息文本
+     */
+    function getWaifuWelcome() {
+        // 主页：展示时间相关文本
+        if (location.pathname === "/hugo-stack/") {
+            for (let { hour, text } of jsonData.time) {
+                const now = new Date(),
+                    after = hour.split("-")[0],
+                    before = hour.split("-")[1] || after;
+                if (after <= now.getHours() && now.getHours() <= before) {
+                    return text;
+                }
             }
         }
-    }
-    const text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
-    let from;
-    if (document.referrer !== "") {
-        const referrer = new URL(document.referrer),
-            domain = referrer.hostname.split(".")[1];
-        const domains = {
-            "baidu": "百度",
-            "so": "360搜索",
-            "google": "谷歌搜索"
-        };
-        if (location.hostname === referrer.hostname) return text;
+        // 其他页面：展示欢迎阅读文本
+        const text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
+        // 存在来源，文本拼接来源地址
+        let from;
+        if (document.referrer !== "") {
+            const referrer = new URL(document.referrer),
+                domain = referrer.hostname.split(".")[1];
+            const domains = {
+                "baidu": "百度",
+                "so": "360搜索",
+                "google": "谷歌搜索",
+                "bing": "edge搜索"
+            };
+            if (location.hostname === referrer.hostname) return text;
 
-        if (domain in domains) from = domains[domain];
-        else from = referrer.hostname;
-        return `Hello！来自 <span>${from}</span> 的朋友<br>${text}`;
+            if (domain in domains) from = domains[domain];
+            else from = referrer.hostname;
+            return `Hello！来自 <span>${from}</span> 的朋友<br>${text}`;
+        }
+        return text;
     }
-    return text;
-}
+
+    /**
+     * 展示欢迎信息
+     */
+    function showWaifuWelcome() {
+        showMessage(getWaifuWelcome(), 7000, 11);
+    }
+
+    window.initWidget = initWidget;
+    window.showWaifuWelcome = showWaifuWelcome;
+})()
+
+
